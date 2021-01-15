31e464d7e42601e8fe89814c33f4fde17ed2d85a
Werner Bailer <werner.bailer@joanneum.at>
2021-01-15 10:19:38 +0100

    fixed special character in comment

--- preparepano/createpano.py
+++ preparepano/createpano.py
@@ -3,7 +3,7 @@
 # http://www.atlantis-ar.eu
 #
 # This tool is part of a project that has received funding from the European 
-# Union’s Horizon 2020 research and innovation programme under grant 
+# Union's Horizon 2020 research and innovation programme under grant 
 # agreement No 951900.
 
 # ported and extended code from https://github.com/yindaz/PanoBasic
